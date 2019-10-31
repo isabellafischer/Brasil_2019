62d89d4f857bb957189cf0867aff52e4eba3a1f3
isabellafischer <54290629+isabellafischer@users.noreply.github.com>
2019-10-31 13:27:49 -0300

    Update 3.R

--- PUCR-Assignment-5/3.R
+++ PUCR-Assignment-5/3.R
@@ -1,10 +1,10 @@
 # Isabella Fischer
 
 library(tidyverse)
+library(tidyr)
 swabs = read_tsv("https://osf.io/sh75w/download")
 counts = read_tsv("https://osf.io/vjcdt/download")
 taxa = read_tsv("https://osf.io/z4bfu/download")
 
-
-swabsOfInterest =select(swabs,swab.id)
+swabsOfInterest<-select(swabs, swab.id)
 print(swabsOfInterest)
