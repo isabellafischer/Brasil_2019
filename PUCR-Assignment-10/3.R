6c1160a46625f427ff2b875299be23778be44504
isabellafischer <54290629+isabellafischer@users.noreply.github.com>
2019-10-31 13:34:05 -0300

    Update 3.R

--- PUCR-Assignment-10/3.R
+++ PUCR-Assignment-10/3.R
@@ -1,28 +1,27 @@
 # Isabella Fischer
 
-library(mlr)
 library(tidyverse)
-set.seed(1)
 
-clinicData = as_tibble(read_tsv("https://osf.io/vqsr4/download"))
+clinicalData = as_tibble(read_tsv("https://osf.io/vqsr4/download"))
 geneExpe = as_tibble(read_tsv("https://osf.io/um4g9/download"))
 
-clinicData$Ethnicity = factor(clinicData$Ethnicity)
-clinicData$Sex = factor(clinicData$Sex)
+clinicalData$Ethnicity = factor(clinicalData$Ethnicity)
+clinicalData$Sex = factor(clinicalData$Sex)
 
 classify = function(tib, target, algoritm, perform){
+  library(mlr)
   task = makeClassifTask(data = tib, target = target)
   learner = makeLearner(algoritm, predict.type = "prob")
-
+  set.seed(1)
   resampleDesc = makeResampleDesc("CV", iters = 5)
-  
   results = resample(learner, task, resampleDesc, show.info = F)
   metrics = performance(results$pred, measures = perform)
   return(metrics)
 }
 
-print(classify(clinicData, "Class", "classif.svm", list(mlr::acc, auc, f1, mcc) ))
-print(classify(clinicData, "Class", "classif.randomForest", list(mlr::acc, auc, f1, mcc) ))
-print(classify(clinicData, "Class", "classif.naiveBayes", list(mlr::acc, auc, f1, mcc) ))
-print(classify(clinicData, "Class", "classif.kknn", list(mlr::acc, auc, f1, mcc) ))
-print(classify(clinicData, "Class", "classif.gbm", list(mlr::acc, auc, f1, mcc) ))
+print(classify(clinicalData, "Class", "classif.svm", list(mlr::acc, auc, f1, mcc) ))
+print(classify(clinicalData, "Class", "classif.randomForest", list(mlr::acc, auc, f1, mcc) ))
+print(classify(clinicalData, "Class", "classif.naiveBayes", list(mlr::acc, auc, f1, mcc) ))
+print(classify(clinicalData, "Class", "classif.kknn", list(mlr::acc, auc, f1, mcc) ))
+print(classify(clinicalData, "Class", "classif.gbm", list(mlr::acc, auc, f1, mcc) ))
+
